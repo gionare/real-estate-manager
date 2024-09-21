d01a734dad5500230c956e97aa9697895969fd53
gionare <gionare93@gmail.com>
2024-09-21 03:24:59 -0400

    remove unnecessary lines

--- src/components/HomePage/CardGrid.tsx
+++ src/components/HomePage/CardGrid.tsx
@@ -1,19 +1,3 @@
-// const CardGrid: React.FC = () => {
-//   // Array with 8 elements to render 8 Card components
-//   const cards = Array(8).fill(null);
-
-//   return (
-//     <div className="grid grid-cols-4 gap-[20px] p-[16px] max-w-[1600px] mx-auto">
-//       {cards.map((_, index) => (
-//         <Card key={index} />
-//       ))}
-//     </div>
-//   );
-// };
-
-// export default CardGrid;
-// src/components/CardGrid.tsx
-
 import React, { useEffect, useState } from "react";
 import { getRealEstates } from "../../services/api";
 import Card from "./Card";
--- src/components/HomePage/Filters.tsx
+++ src/components/HomePage/Filters.tsx
@@ -1,6 +1,6 @@
 // import React from "react";
 
-import React, { useState } from "react";
+import { useState } from "react";
 import PriceCategory from "./Filters/PriseCategory";
 
 const FilterComponent = () => {
